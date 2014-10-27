927569c0a4c3cbe0ec1211ddc3305642668c1ddf
SamiTeixeira <samilateixeira2013@gmail.com>
2014-10-27 11:49:52 -0700

    Modificando o arquivo routes

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,8 @@ Rails.application.routes.draw do
 
   resources :tipo_de_imovels
 
+  root 'proprietarios#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
